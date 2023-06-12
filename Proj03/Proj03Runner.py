8466d2a9c5b663d28f9733a92b4d8438eaae6038
Sam Haskins <sam.haskins@g.austincc.edu>
2023-06-12 14:22:22 -0500

    actually done this time - LGTM!

--- Proj03/Proj03Runner.py
+++ Proj03/Proj03Runner.py
@@ -5,106 +5,75 @@ import matplotlib.pyplot as plt
 import math
 
 
-def normalRandomGenerator(seed=1, dataLength=10000, numberSamples=50, lowLim=0, highLim=100):
+def normal_random_generator(seed=1, data_length=10000, number_samples=50, low_lim=0, high_lim=100):
     """
-    Generate random data from a normal distribution.
-
-    Args:
-        seed (int): Seed for the random number generator.
-        dataLength (int): Length of the generated data.
-        numberSamples (int): Number of samples used to compute the average at each step.
-        lowLim (float): Lower limit of the uniform distribution.
-        highLim (float): Upper limit of the uniform distribution.
-
-    Returns:
-        list: Randomly generated data from a normal distribution.
-
+    generates a list of random numbers from a normal distribution
     """
     data = []
     np.random.seed(seed)
 
-    for cnt in range(dataLength):
-        theSum = 0
-        for cnt1 in range(numberSamples):
-            theSum += random.uniform(lowLim, highLim)
-        data.append(theSum / numberSamples)
+    for cnt in range(data_length):
+        the_sum = 0
+        for cnt1 in range(number_samples):
+            the_sum += random.uniform(low_lim, high_lim)
+        data.append(the_sum / number_samples)
 
     return data
 
 
-def other_data(seed, dataLength, numberSamples, lowLim, highLim):
+def other_data(seed, data_length, number_samples, low_lim, high_lim):
+    """
+    generates additional, positively skewed data using a uniform distribution
+    """
     np.random.seed(seed)
-
-    new_data = np.random.uniform(low=lowLim, high=highLim, size=(dataLength, numberSamples))
+    new_data = np.random.uniform(low=low_lim, high=high_lim, size=(data_length, number_samples))
     new_avg = np.mean(new_data, axis=1)
     new_skew = np.sqrt(new_avg)
     return new_skew.tolist()
 
 
-def lmsBestFitLine(xDataIn, yDataIn):
+def lms_best_fit_line(x_data_in, y_data_in):
     """
-    Compute the parameters of the best-fit line using the Least Mean Squares (LMS) method.
-
-    Args:
-        xDataIn (numpy.ndarray): Input data for the x-axis.
-        yDataIn (numpy.ndarray): Input data for the y-axis.
-
-    Returns:
-        tuple: Slope (m) and intercept (b) of the best-fit line.
-
+    calculates the best-fit line using the least mean squares (LMS) method
     """
-    xBar = np.mean(xDataIn)
-    yBar = np.mean(yDataIn)
+    x_bar = np.mean(x_data_in)
+    y_bar = np.mean(y_data_in)
 
-    numerator = np.sum((xDataIn - xBar) * (yDataIn - yBar))
-    denominator = np.sum((xDataIn - xBar) ** 2)
+    numerator = np.sum((x_data_in - x_bar) * (y_data_in - y_bar))
+    denominator = np.sum((x_data_in - x_bar) ** 2)
 
     m = numerator / denominator
-    b = yBar - m * xBar
+    b = y_bar - m * x_bar
 
     return m, b
 
 
-def standardNormalDistribution(x):
+def standard_normal_distribution(x):
     """
-    Compute the value of the standard normal distribution at a given point.
-
-    Args:
-        x (float): Input value.
-
-    Returns:
-        float: Value of the standard normal distribution at the given point.
-
+    calculates the value of the standard normal distribution at a given point
     """
-    eVal = 2.718281828459045
+    e_val = 2.718281828459045
     exp = (-x ** 2) / 2
-    numerator = pow(eVal, exp)
+    numerator = pow(e_val, exp)
     denominator = math.sqrt(2 * math.pi)
     return numerator / denominator
 
 
-def normalProbabilityDensity(x, mu=0, sigma=1.0):
+def normal_probability_density(x, mu=0, sigma=1.0):
     """
-    Compute the value of the normal probability density function at a given point.
-
-    Args:
-        x (float): Input value.
-        mu (float): Mean of the distribution. Default is 0.
-        sigma (float): Standard deviation of the distribution. Default is 1.0.
-
-    Returns:
-        float: Value of the normal probability density function at the given point.
-
+    calculates the value of the normal probability density function at a given point
     """
-    eVal = 2.718281828459045
+    e_val = 2.718281828459045
     exp = -((x - mu) ** 2) / (2 * (sigma ** 2))
-    numerator = pow(eVal, exp)
+    numerator = pow(e_val, exp)
     denominator = sigma * (math.sqrt(2 * math.pi))
     return numerator / denominator
 
 
-def cumulativeDistribution(x):
-    # constants
+def cumulative_distribution(x):
+    """
+    calculates the cumulative distribution function at a given point
+    """
     a1 = 0.254829592
     a2 = -0.284496736
     a3 = 1.421413741
@@ -112,74 +81,61 @@ def cumulativeDistribution(x):
     a5 = 1.061405429
     p = 0.3275911
 
-    # Save the sign of x
     sign = 1
     if x < 0:
         sign = -1
     x = abs(x) / math.sqrt(2.0)
 
-    # A&S formula 7.1.26
     t = 1.0 / (1.0 + p * x)
     y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * math.exp(-x * x)
 
     return 0.5 * (1.0 + sign * y)
 
 
-print("I certify that this program is my own work and is not the work of others.")
-print("I agree not to share my solution with others.")
-print("Sam Beers Haskins")
-
-
 class Runner:
     def run():
         """
-        Run the program and generate the plots.
-
+        runs the main code for generating plots and visualizations
         """
-        # Create a figure with two rows and two columns.
         fig, ax = plt.subplots(2, 2)
 
-        xData = normalRandomGenerator(seed=1, dataLength=100, numberSamples=1, lowLim=0, highLim=200)
-        noise = normalRandomGenerator(seed=2, dataLength=200, numberSamples=1, lowLim=0, highLim=10)
+        x_data = normal_random_generator(seed=1, data_length=100, number_samples=1, low_lim=0, high_lim=200)
+        noise = normal_random_generator(seed=2, data_length=200, number_samples=1, low_lim=0, high_lim=10)
 
-        yData = []
-        for cnt in range(len(xData)):
-            yData.append(xData[cnt] / 10 + noise[cnt])
+        y_data = []
+        for cnt in range(len(x_data)):
+            y_data.append(x_data[cnt] / 10 + noise[cnt])
 
-        m, b = lmsBestFitLine(xData, yData)
+        m, b = lms_best_fit_line(x_data, y_data)
 
-        x1 = min(xData)
+        x1 = min(x_data)
         y1 = m * x1 + b
-        x2 = max(xData)
+        x2 = max(x_data)
         y2 = m * x2 + b
 
-        # Plot a box in the upper left quadrant
+        # plot in the upper left quadrant
         ax[0, 0].plot([x1, x2], [y1, y2])
-        ax[0, 0].scatter(xData, yData, c='red', s=50, edgecolor='blue')
+        ax[0, 0].scatter(x_data, y_data, c='red', s=50, edgecolor='blue')
         ax[0, 0].grid(True)
 
-        # Create an array of uniformly spaced values, -3 sigma to +3 sigma
         x = np.arange(-3, 3, 0.1)
-        # Create a list containing a distribution value for each value in x
-        y = [standardNormalDistribution(val) for val in x]
+        y = [standard_normal_distribution(val) for val in x]
 
-        # Use plot in the upper right quadrant
+        # plot in the upper right quadrant
         ax[0, 1].plot(x, y)
         ax[0, 1].grid(True)
 
-        # Plot a positively skewed histogram in lower left quadrant
-        data = other_data(seed=1, dataLength=2000, numberSamples=20, lowLim=0, highLim=2500)
+        # plot in the lower left quadrant, using positively skewed data
+        data = other_data(seed=1, data_length=2000, number_samples=20, low_lim=0, high_lim=2500)
         ax[1, 0].hist(data, bins=50)
         ax[1, 0].grid(True)
 
-        # Plot a flipped histogram in the lower right quadrant
         x = np.arange(-3, 3, 0.1)
-        y = [cumulativeDistribution(val) for val in x]
+        y = [cumulative_distribution(val) for val in x]
 
-        # Use matplotlib to plot the data.
+        # plot in the lower right quadrant
         ax[1, 1].plot(x, y)
 
-        # Add title to the figure and show it
-        fig.suptitle('Sam Beers Haskins')
+        fig.suptitle('sam beers haskins')
         plt.tight_layout()
         plt.show()
