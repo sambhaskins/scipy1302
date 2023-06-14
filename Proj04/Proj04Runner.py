365ba92bfb435a9b8791fee2df1c1fa1c9219927
Sam Haskins <sam.haskins@g.austincc.edu>
2023-06-14 16:18:35 -0500

    LGTM!

--- Proj04/Proj04Runner.py
+++ Proj04/Proj04Runner.py
@@ -1,109 +1,112 @@
+from statistics import mean, stdev
 import matplotlib.pyplot as plt
 import numpy as np
-from statistics import mean, stdev
 from matplotlib.ticker import FormatStrFormatter
+
+"""
+ We'll use and repurpose the normal_probability_function from the textbook. I changed the function to follow 
+ the snake_case naming convention, out of personal preference. This function wil be used to calculate the normal
+ probability density of our dataset(s). I have used np.linspace() to generate evenly spaced points to plot the 
+ normal probability density line.Furthermore, I have set the `num` value to 1000, to generate more points, 
+ hopefully creating more precision. I used `markevery` to plot the datapoints in respect to the required output
+   in Figure 2.
+"""
+
+
+def normal_probability_density(x, mu=0, sigma=2.0):
+    e_val = 2.718281828459045
+    exp = -((x - mu) ** 2) / (2 * (sigma ** 2))
+    numerator = pow(e_val, exp)
+    denominator = sigma * (np.sqrt(2 * np.pi))
+    return numerator / denominator
 
 
 class Runner:
-    def run(data01, data02, data03, data04):
-        # Create a 2x2 figure containing four plots with shaorange x and y tick marks and labels.
+    def run(data_01, data_02, data_03, data_04):
+        # Make a 2x2 figure containing four plots with shared x and y tick marks and labels
         fig, ax = plt.subplots(2, 2, sharey=True, sharex=True)
 
-        # Plot data01
+        # Plot first batch of data in upper left quadrant
         cnt, bins, _ = ax[0, 0].hist(
-            data01,
+            data_01,
             bins=50,
             density=True,
-            range=(min(data01), max(data01))
+            range=(min(data_01), max(data_01))
         )
-        data_mean = mean(data01)
-        data_std = stdev(data01)
-        x = np.linspace(min(data01), max(data01), 100)
-        y = (1 / (data_std * np.sqrt(2 * np.pi))) * \
-            np.exp(-(x - data_mean)**2 / (2 * data_std**2))
-        line, = ax[0, 0].plot(x, y, color='orange', marker='.', markevery=10)
+        data_mean = mean(data_01)
+        data_std = stdev(data_01)
+        x = np.linspace(min(data_01), max(data_01), num=1000)
+        y = normal_probability_density(x, data_mean, data_std)
+        line, = ax[0, 0].plot(x, y, color='orange', marker='.', markevery=100)
         ax[0, 0].legend([line], ['norm prob density'], loc='upper left')
         ax[0, 0].grid(True)
         ax[0, 0].set_title('Upper Left')
         ax[0, 0].set_ylabel('Relative Frequency')
-        ax[0, 0].yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
 
-
-
-        # Plot data02
+        # Plot second batch of data in upper right quadrant
         cnt, bins, _ = ax[0, 1].hist(
-            data02,
+            data_02,
             bins=50,
             density=True,
-            range=(min(data02), max(data02))
+            range=(min(data_02), max(data_02))
         )
-        data_mean = mean(data02)
-        data_std = stdev(data02)
-        x = np.linspace(min(data02), max(data02), 100)
-        y = (1 / (data_std * np.sqrt(2 * np.pi))) * \
-            np.exp(-(x - data_mean)**2 / (2 * data_std**2))
-        line, = ax[0, 1].plot(x, y, color='orange', marker='.', markevery=5)
+        data_mean = mean(data_02)
+        data_std = stdev(data_02)
+        x = np.linspace(min(data_02), max(data_02), num=1000)
+        y = normal_probability_density(x, data_mean, data_std)
+        line, = ax[0, 1].plot(x, y, color='orange', marker='.', markevery=50)
         ax[0, 1].legend([line], ['norm prob density'], loc='upper left')
-        ax[0, 1].plot(x, y, color='orange')
         ax[0, 1].grid(True)
         ax[0, 1].set_title('Upper Right')
-        ax[0, 1].yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
 
-
-        # Plot data03
+        # Plot third batch of data in lower left quadrant
         cnt, bins, _ = ax[1, 0].hist(
-            data03,
+            data_03,
             bins=50,
             density=True,
-            range=(min(data03), max(data03))
+            range=(min(data_03), max(data_03))
         )
-        data_mean = mean(data03)
-        data_std = stdev(data03)
-        x = np.linspace(min(data03), max(data03), 100)
-        y = (1 / (data_std * np.sqrt(2 * np.pi))) * \
-            np.exp(-(x - data_mean)**2 / (2 * data_std**2))
-        line, = ax[1, 0].plot(x, y, color='orange', marker='.', markevery=3)
+        data_mean = mean(data_03)
+        data_std = stdev(data_03)
+        x = np.linspace(min(data_03), max(data_03), num=1000)
+        y = normal_probability_density(x, data_mean, data_std)
+        line, = ax[1, 0].plot(x, y, color='orange', marker='.', markevery=25)
         ax[1, 0].legend([line], ['norm prob density'], loc='upper left')
         ax[1, 0].grid(True)
         ax[1, 0].set_title('Lower Left')
         ax[1, 0].set_xlabel('x')
         ax[1, 0].set_ylabel('Relative Frequency')
-        ax[1, 0].yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
 
-
-        # Plot data04
+        # Plot the fourth and final batch of data in the lower left quadrant
         cnt, bins, _ = ax[1, 1].hist(
-            data04,
+            data_04,
             bins=50,
             density=True,
-            range=(min(data04), max(data04))
+            range=(min(data_04), max(data_04))
         )
-        data_mean = mean(data04)
-        data_std = stdev(data04)
-        x = np.linspace(min(data04), max(data04), 100)
-        y = (1 / (data_std * np.sqrt(2 * np.pi))) * \
-            np.exp(-(x - data_mean)**2 / (2 * data_std**2))
-        line, = ax[1, 1].plot(x, y, color='orange', marker='.', markevery=2)
+        data_mean = mean(data_04)
+        data_std = stdev(data_04)
+        x = np.linspace(min(data_04), max(data_04), num=1000)
+        y = normal_probability_density(x, data_mean, data_std)
+        line, = ax[1, 1].plot(x, y, color='orange', marker='.', markevery=20)
         ax[1, 1].legend([line], ['norm prob density'], loc='lower left')
         ax[1, 1].grid(True)
         ax[1, 1].set_title('Lower Right')
         ax[1, 1].set_xlabel('x')
-        ax[1, 1].yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
 
-
-        # Display the number of bins in all histograms
+        # Display the number of bins across all histograms, subract one to account for the bin edges
         num_bins = len(bins) - 1
 
-        # Display the number of all bins in all histograms
+        # Certification
         print("I certify that this program is my own work and not the work of others")
         print("I agree not to share my solution with others")
         print("Sam Beers Haskins")
         print()
         print(f"Number of bins in all histograms = {num_bins}")
 
-        # Display a title at the top
+        # Display a title at top of output window
         plt.suptitle("Sam Beers Haskins")
 
-        # Cause the plot to become visible
+        # Display plots
         plt.tight_layout()
         plt.show()
