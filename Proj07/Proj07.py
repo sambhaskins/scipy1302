c69d0fb2f057afa2f55b8c22755e7efc4beb876d
Sam Haskins <sam.haskins@g.austincc.edu>
2023-06-23 13:51:12 -0700

    initial commit, lgtm

---
+++ Proj07/Proj07.py
@@ -0,0 +1,17 @@
+#DO NOT MODIFY THE CODE IN THIS FILE
+#File: Proj05.py
+#Rev 12/05/22
+'''
+This assignment requires you to demonstrate your knowledge 
+of the material covered under "Spines and text" in 
+"Visualization Exercises Part 2" as well as material 
+leading up to that topic.
+'''
+
+from Proj07Runner import Runner
+#Call the student's run method.
+Runner.run()
+
+
+
+
---
+++ Proj07/Proj07Runner.py
@@ -0,0 +1,6 @@
+import matplotlib as plt
+import numpy as np
+
+class Runner:
+    def run():
+        
