8c07ab1efd108e8f9f349d905a56838e063e33a1
Sam Haskins <sam.haskins@g.austincc.edu>
2023-06-25 17:41:20 -0700

    👍

--- Proj07/Proj07Runner.py
+++ Proj07/Proj07Runner.py
@@ -1,6 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+"""
+This code will display a 2x2 plot with each subplot representing a different mathematical function.
+We'll work on producing & graphing the following functions:
+
+1. Sine function
+2. 2*Cosine function
+3. Negative cubic function
+4. Half-square function
+
+The code in this program covers lessons / exercises including Visualizations Chapter 2.
+"""
 
 class Runner:
     def run():
@@ -10,15 +21,15 @@ class Runner:
         print("Sam Beers Haskins")
 
         # Setup 2x2 subplots with specific dimensions and edge styles
-        fig,axes = plt.subplots(2,2,
-                        facecolor='0.75',
-                        linewidth=10,
-                        edgecolor='Green')
+        fig,axes = plt.subplots(2,2, facecolor='0.75', linewidth=10, edgecolor='Green')
         
-        # Unpack the 2D array into separate var iables for easier access
+        # Create a range of t values for the plots
         t = np.arange(-3.0, 3.0, 0.05)
-        ax0,ax1,ax2,ax3 = axes.flatten() 
+        
+        # Unpack the 2D array into separate variables for easier access
+        ax0, ax1, ax2, ax3 = axes.flatten()
 
+        # Plot Sine function on the first subplot (upper left)
         ax0.spines['left'].set_position('center')
         ax0.spines['right'].set_color('none')
         ax0.spines['bottom'].set_position('center')
@@ -27,17 +38,16 @@ class Runner:
         ax0.text(-3,0.5,'$Sine$')
         ax0.plot(t,np.sin(1*np.pi*t))
 
-        # 2 * Cos plot
+        # Plot 2*Cosine function on the second subplot (upper right)
         ax1.spines['left'].set_position('center')
         ax1.spines['right'].set_color('none')
         ax1.spines['bottom'].set_position('center')
         ax1.spines['top'].set_color('none')
         ax1.set_xticks(np.arange(-2,3,2))
-        ax1.text(-3,1,'$2*Cosine$')  # Change 'sine' to '2cosine'
-        ax1.plot(t, 2*np.cos(0.5*np.pi*t))  # Change np.sin to 2*np.cos
+        ax1.text(-3,1,'$2*Cosine$')
+        ax1.plot(t, 2*np.cos(0.5*np.pi*t))
 
-
-        # -t^3 plot
+        # Plot Negative cubic function on the third subplot (lower left)
         ax2.plot(t, -t**3, label='')
         ax2.spines['left'].set_position('center')
         ax2.spines['bottom'].set_position('center')
@@ -45,9 +55,9 @@ class Runner:
         ax2.spines['top'].set_color('none')
         ax2.xaxis.set_ticks_position('bottom')
         ax2.yaxis.set_ticks_position('left')
-        ax2.text(1, 8, '$y=-x^3$')
+        ax2.text(.5, 8, '$y=-x^3$')
 
-        # 0.5 * t^2 plot
+        # Plot Half-square function on the fourth subplot (lower right)
         ax3.plot(t, 0.5 * t**2)
         ax3.spines['left'].set_position('center')
         ax3.spines['bottom'].set_position('center')
@@ -58,6 +68,6 @@ class Runner:
         ax3.text(0.25, 3.5, '$y=0.5*x^2$')
         ax3.set_ylim(-4.5,4.5)
 
-        # Improve the layout
+        # Render plot & display my name in bottom left corner
         plt.figtext(0.1,0.02,'Sam Beers Haskins')
         plt.show()
