4c0b953a99176510794c93ffa1676fcefa4e150a
Sam Haskins <sam.haskins@g.austincc.edu>
2023-06-25 17:28:46 -0700

    👍

--- Proj07/Proj07Runner.py
+++ Proj07/Proj07Runner.py
@@ -10,62 +10,56 @@ class Runner:
         print("Sam Beers Haskins")
 
         # Setup 2x2 subplots with specific dimensions and edge styles
-        fig, ax = plt.subplots(2, 2, figsize=(
-            7, 5), facecolor='0.75', linewidth=10, edgecolor='Green')
+        fig,axes = plt.subplots(2,2,
+                        facecolor='0.75',
+                        linewidth=10,
+                        edgecolor='Green')
         
         # Unpack the 2D array into separate var iables for easier access
         t = np.arange(-3.0, 3.0, 0.05)
+        ax0,ax1,ax2,ax3 = axes.flatten() 
 
-        ax[0, 0], ax[0, 1], ax[1, 0], ax[1, 1] = ax.flatten()
-
-        # Sin plot
-        ax[0, 0].plot(t, np.sin(t), label='A Sine')
-        ax[0, 0].spines['left'].set_position('center')
-        ax[0, 0].spines['bottom'].set_position('center')
-        ax[0, 0].spines['right'].set_color('none')
-        ax[0, 0].spines['top'].set_color('none')
-        ax[0, 0].xaxis.set_ticks_position('bottom')
-        ax[0, 0].yaxis.set_ticks_position('left')
-        ax[0, 0].set_title('y = sin(t)')
-        ax[0, 0].text(-2, 0.5, 'sin(t)', fontsize=12)
-        ax[0, 0].legend()
+        ax0.spines['left'].set_position('center')
+        ax0.spines['right'].set_color('none')
+        ax0.spines['bottom'].set_position('center')
+        ax0.spines['top'].set_color('none')
+        ax0.set_xticks(np.arange(-2,3,2))
+        ax0.text(-3,0.5,'$Sine$')
+        ax0.plot(t,np.sin(1*np.pi*t))
 
         # 2 * Cos plot
-        ax[0, 1].plot(t, 2 * np.cos(t), label='2*Cosine', color='orange')
-        ax[0, 1].spines['left'].set_position('center')
-        ax[0, 1].spines['bottom'].set_position('center')
-        ax[0, 1].spines['right'].set_color('none')
-        ax[0, 1].spines['top'].set_color('none')
-        ax[0, 1].xaxis.set_ticks_position('bottom')
-        ax[0, 1].yaxis.set_ticks_position('left')
-        ax[0, 1].set_title('y = 2cos(t)')
-        ax[0, 1].text(-2, 1, '2cos(t)', fontsize=12)
-        ax[0, 1].legend()
+        ax1.spines['left'].set_position('center')
+        ax1.spines['right'].set_color('none')
+        ax1.spines['bottom'].set_position('center')
+        ax1.spines['top'].set_color('none')
+        ax1.set_xticks(np.arange(-2,3,2))
+        ax1.text(-3,1,'$2*Cosine$')  # Change 'sine' to '2cosine'
+        ax1.plot(t, 2*np.cos(0.5*np.pi*t))  # Change np.sin to 2*np.cos
+
 
         # -t^3 plot
-        ax[1, 0].plot(t, -t**3, label='-t^3', color='green')
-        ax[1, 0].spines['left'].set_position('center')
-        ax[1, 0].spines['bottom'].set_position('center')
-        ax[1, 0].spines['right'].set_color('none')
-        ax[1, 0].spines['top'].set_color('none')
-        ax[1, 0].xaxis.set_ticks_position('bottom')
-        ax[1, 0].yaxis.set_ticks_position('left')
-        ax[1, 0].set_title('y = -t^3')
-        ax[1, 0].text(-2, 2, '-t^3', fontsize=12)
-        ax[1, 0].legend()
+        ax2.plot(t, -t**3, label='')
+        ax2.spines['left'].set_position('center')
+        ax2.spines['bottom'].set_position('center')
+        ax2.spines['right'].set_color('none')
+        ax2.spines['top'].set_color('none')
+        ax2.xaxis.set_ticks_position('bottom')
+        ax2.yaxis.set_ticks_position('left')
+        ax2.text(1, 8, '$y=-x^3$')
+        ax2.legend()
 
         # 0.5 * t^2 plot
-        ax[1, 1].plot(t, 0.5 * t**2, label='0.5t^2', color='red')
-        ax[1, 1].spines['left'].set_position('center')
-        ax[1, 1].spines['bottom'].set_position('center')
-        ax[1, 1].spines['right'].set_color('none')
-        ax[1, 1].spines['top'].set_color('none')
-        ax[1, 1].xaxis.set_ticks_position('bottom')
-        ax[1, 1].yaxis.set_ticks_position('left')
-        ax[1, 1].set_title('y = 0.5t^2')
-        ax[1, 1].text(-2, 1, '0.5t^2', fontsize=12)
-        ax[1, 1].legend()
+        ax3.plot(t, 0.5 * t**2)
+        ax3.spines['left'].set_position('center')
+        ax3.spines['bottom'].set_position('center')
+        ax3.spines['right'].set_color('none')
+        ax3.spines['top'].set_color('none')
+        ax3.xaxis.set_ticks_position('bottom')
+        ax3.yaxis.set_ticks_position('left')
+        ax3.text(0.25, 3.5, '$y=0.5*x^2$')
+        ax3.set_ylim(-4.5,4.5)
+        ax3.legend()
 
         # Improve the layout
-        plt.tight_layout()
+        plt.figtext(0.1,0.02,'Sam Beers Haskins')
         plt.show()
