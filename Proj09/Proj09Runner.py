dbf915006c1de8b5561dbb82578c1e169ea5c9e2
Sam Haskins <sam.haskins@g.austincc.edu>
2023-06-28 17:02:28 -0700

    👍

--- Proj09/Proj09Runner.py
+++ Proj09/Proj09Runner.py
@@ -9,14 +9,14 @@ class Runner:
         print("I agree not to share my solution with others")
         print("Sam Beers Haskins")
 
-        fig,ax0 = plt.subplots(1,1)
+        fig,ax0 = plt.subplots(1,1,
+                            facecolor='0.75',
+                            linewidth=3,
+                            edgecolor='Black')
 
         t = np.arange(0, 6.0, 0.11)
-        ax0.plot(t, np.exp(-t/2) * np.cos(np.pi*t),'-.',
+        line0,line1 = ax0.plot(t, np.exp(-t/2) * np.cos(np.pi*t),'-.',
                             t, np.cos(np.pi*t),'--o')
-
-        ax0.set_ylabel('This is a ylabel')
-        ax0.set_xlabel('This is an xlabel')
 
         ax0.xaxis.set_major_locator(ticker.MultipleLocator(0.5))
         ax0.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
@@ -28,8 +28,12 @@ class Runner:
         ax0.tick_params(axis='both',which='major',length=10,
                         labelcolor='blue',width=2)
 
-        ax0.grid(b=True, which='major', color='cyan')
-        ax0.grid(b=True, which='minor', color='blue',
+        ax0.grid(visible=True, which='major', color='cyan')
+        ax0.grid(visible=True, which='minor', color='blue',
                 linestyle='dotted',alpha=0.6)
+
+        ax0.set_ylabel('Sam')
+        ax0.set_xlabel('Haskins')
+        ax0.grid(True)
 
         plt.show()
