a3067d199c8754340a375e852eb562298251dea8
Sam Haskins <sam.haskins@g.austincc.edu>
2023-07-06 13:47:41 -0700

    adding stuff

---
+++ Proj12/Proj12.py
@@ -0,0 +1,19 @@
+# DO NOT MODIFY THE CODE IN THIS FILE
+# File: Proj12.py
+# Rev 12/05/2
+'''
+This assignment requires you to write code demonstrating 
+your knowledge of bar charts with multiple x-values.
+'''
+
+from Proj12Runner import Runner
+
+# Create a dataset
+names = ['Sue', 'Tom', 'Bill', 'Joe', 'Dick']
+
+test01 = [48, 50, 60, 46, 62]
+test02 = [34, 70, 28, 54, 36]
+test03 = [100, 62, 72, 60, 75]
+
+# Call student's run method passing the data as parameters.
+Runner.run(names, test01, test02, test03)
