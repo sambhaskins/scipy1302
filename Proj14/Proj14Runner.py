990d120f2147d4b4fb2b6679d6e8c2c2ada68964
Sam Haskins <sam.haskins@g.austincc.edu>
2023-07-13 14:41:03 -0500

    doneski

--- Proj14/Proj14Runner.py
+++ Proj14/Proj14Runner.py
@@ -10,6 +10,16 @@ class Runner:
         print("Sam Beers Haskins")
 
     def run(list_args, array_2d):
+        """
+        This function takes a list and a 2D array as input. It returns a new list and 2D array, where 
+        the new list is the original list duplicated and the 2D array is the original array with each 
+        element multiplied by 2.
+        """
+        # Duplicates the list_args
         r1 = list_args * 2
-        r2 = array_2d + list_args
+
+        # Mulitiply each element in the array by 2
+        r2 = array_2d * 2
+
+        # Return the new list and array
         return r1, r2
