ba15b8011864540ad4a7b4d754a802d8abf1d342
Sam Haskins <sam.haskins@g.austincc.edu>
2023-07-13 11:36:04 -0500

    basically done but need a response on the shadow situation

--- Proj13/Proj13Runner.py
+++ Proj13/Proj13Runner.py
@@ -1,5 +1,9 @@
-import numpy as np
 import matplotlib.pyplot as plt
+
+"""
+This program visualizes a set of test scores using a pie chart. Each slice of the pie represents the score 
+for a particular test. Two of the tests are emphasized with "exploded" slices.
+"""
 
 
 class Runner:
@@ -10,13 +14,27 @@ class Runner:
         print("I agree not to share my solution with others")
         print("Sam Beers Haskins")
 
-        tests = 'Game 1', 'Game 2', 'Game 3', 'Game 4', 'Game 5', 'Game 6', 'Game 7'
-        explodeSlices = (0, 0, 0, 0, 0.3, 0, 0.2)  # explode largest and smallest
-        percentages = '%1.1f%%'  # show percentage of each slice
+        # Define the labels for each test
+        tests = 'Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5', 'Test 6', 'Test 7'
 
+        # Define which slices to "explode" for emphasis
+        explodeSlices = (0, 0, 0, 0, 0.3, 0, 0.3)
+
+        # Define the format for the percentage labels on each slice
+        percentages = '%1.1f%%'
+
+        # Create a new figure and a single subplot
         fig1, ax = plt.subplots()
+
+        # Create the pie chart
         ax.pie(scores, explode=explodeSlices, labels=tests, autopct=percentages,
                shadow=True, startangle=90)
-        ax.axis('equal')  # ensure that pie is drawn as a circle.
+
+        # Ensure that the pie chart is drawn as a circle (i.e., the aspect ratio is equal)
+        ax.axis('equal')
+
+        # Set the title of the pie chart
         ax.set_title('Sam Beers Haskins')
+
+        # Display the pie chart
         plt.show()
